22d42dd190fe9642dd29aba08100ad2c62b1b3f9
Alessandro Aronica <ale.aronica@outlook.it>
2026-06-28 11:20:11 +0200

    updated assets

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,31 +1,27 @@
-import { Col, Row } from 'react-bootstrap'
-import classes from './Hero.module.css'
-import hero from '../../assets/hero.png'
-import Typewriter from 'typewriter-effect';
-import { useLanguage } from '../../contexts/LanguageContext';
+import { Col, Row } from "react-bootstrap";
+import classes from "./Hero.module.css";
+import hero from "../../assets/hero.png";
+import Typewriter from "typewriter-effect";
+import { useLanguage } from "../../contexts/LanguageContext";
 
 const Hero = () => {
   const { content } = useLanguage();
 
   return (
-    <Row
-      className='align-items-center'
-    >
-      <Col
-        xs={12}
-        md={6}
-      >
-        <div
-          className={classes['hero-main-container']}
-        >
-          <h1
-            className={classes['greeting']}
-          >{content.hero.greeting}<span>!</span></h1>
-          <div className={classes['intro-container']}>
-            <div className={classes['accent-line']}></div>
-            <h1 className={classes['introduction']}>{content.hero.introduction}</h1>
+    <Row className="align-items-center">
+      <Col xs={12} md={6}>
+        <div className={classes["hero-main-container"]}>
+          <h1 className={classes["greeting"]}>
+            {content.hero.greeting}
+            <span>!</span>
+          </h1>
+          <div className={classes["intro-container"]}>
+            <div className={classes["accent-line"]}></div>
+            <h1 className={classes["introduction"]}>
+              {content.hero.introduction}
+            </h1>
           </div>
-          <h1 className={classes['job']}>
+          <h1 className={classes["job"]}>
             <span>{`<`}</span>
             {/* 
             <Typewriter
@@ -53,22 +49,17 @@ const Hero = () => {
           </h1>
         </div>
       </Col>
-      <Col
-        xs={12}
-        md={6}
-      >
-        <div
-          className={classes['hero-img-container']}
-        >
+      <Col xs={12} md={6}>
+        <div className={classes["hero-img-container"]}>
           <img
-            className='w-100 object-fit-cover d-block'
+            className="w-100 object-fit-cover d-block"
             src={hero}
             alt="profile picture"
           />
         </div>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,62 @@
-import { Col, Container, NavDropdown, Row } from 'react-bootstrap'
-import logo from '../../assets/logo2.png'
-import classes from './Navbar.module.css'
-import { useLanguage } from '../../contexts/LanguageContext'
+import { Col, Container, NavDropdown, Row } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+import classes from "./Navbar.module.css";
+import { useLanguage } from "../../contexts/LanguageContext";
 
 const Navbar = () => {
-
   const { language, changeLanguage, content } = useLanguage();
 
   const flagMap = {
-    en: 'gb',
-    it: 'it'
+    en: "gb",
+    it: "it",
   };
 
   return (
-    <Container fluid className={classes['nav-main-container']}>
-      <Row
-        className='align-items-center py-3 w-100'
-      >
-        <Col
-          xs={12}
-          lg={10}
-          className='mx-auto'
-        >
-          <div className='d-flex justify-content-between align-items-center'>
-            <div
-              className={classes['logo-container']}
-            >
-              <img className='w-100 d-block object-fit-cover' src={logo} alt="logo" />
+    <Container fluid className={classes["nav-main-container"]}>
+      <Row className="align-items-center py-3 w-100">
+        <Col xs={12} lg={10} className="mx-auto">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className={classes["logo-container"]}>
+              <img
+                className="w-100 d-block object-fit-cover"
+                src={logo}
+                alt="logo"
+              />
             </div>
 
             <div className="d-flex align-items-center gap-4">
-              <ul className={`${classes['nav-links']} d-flex list-unstyled m-0 fw-light`}>
-                <li><a href="#services" className="text-decoration-none text-reset">{content.navbar.services}</a></li>
-                <li><a href="#about" className="text-decoration-none text-reset">{content.navbar.about}</a></li>
-                <li><a href="#projects" className="text-decoration-none text-reset">{content.navbar.projects}</a></li>
-                <li><a href="#contact" className="text-decoration-none text-reset">{content.navbar.contact}</a></li>
+              <ul
+                className={`${classes["nav-links"]} d-flex list-unstyled m-0 fw-light`}
+              >
+                <li>
+                  <a
+                    href="#services"
+                    className="text-decoration-none text-reset"
+                  >
+                    {content.navbar.services}
+                  </a>
+                </li>
+                <li>
+                  <a href="#about" className="text-decoration-none text-reset">
+                    {content.navbar.about}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#projects"
+                    className="text-decoration-none text-reset"
+                  >
+                    {content.navbar.projects}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#contact"
+                    className="text-decoration-none text-reset"
+                  >
+                    {content.navbar.contact}
+                  </a>
+                </li>
               </ul>
               <NavDropdown
                 title={
@@ -45,14 +67,30 @@ const Navbar = () => {
                   />
                 }
                 id="language-dropdown"
-                className={classes['lang-dropdown']}
+                className={classes["lang-dropdown"]}
                 align="end"
               >
-                <NavDropdown.Item onClick={() => changeLanguage('en')} className="d-flex align-items-center gap-2">
-                  <img src="https://flagcdn.com/w80/gb.png" width="20" alt="English" /> EN
+                <NavDropdown.Item
+                  onClick={() => changeLanguage("en")}
+                  className="d-flex align-items-center gap-2"
+                >
+                  <img
+                    src="https://flagcdn.com/w80/gb.png"
+                    width="20"
+                    alt="English"
+                  />{" "}
+                  EN
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => changeLanguage('it')} className="d-flex align-items-center gap-2">
-                  <img src="https://flagcdn.com/w80/it.png" width="20" alt="Italiano" /> IT
+                <NavDropdown.Item
+                  onClick={() => changeLanguage("it")}
+                  className="d-flex align-items-center gap-2"
+                >
+                  <img
+                    src="https://flagcdn.com/w80/it.png"
+                    width="20"
+                    alt="Italiano"
+                  />{" "}
+                  IT
                 </NavDropdown.Item>
               </NavDropdown>
             </div>
@@ -60,7 +98,7 @@ const Navbar = () => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
