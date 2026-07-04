9384973def2f6519e6444ecdbc49c1e21d7e7bd0
Alessandro Aronica <ale.aronica@outlook.it>
2026-07-04 13:04:37 +0200

    updated tech stack

--- src/components/techStack/TechStack.jsx
+++ src/components/techStack/TechStack.jsx
@@ -1,65 +1,43 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-import 'swiper/css';
-import classes from './TechStack.module.css'
-import { Container } from 'react-bootstrap';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import classes from "./TechStack.module.css";
+import { Container } from "react-bootstrap";
+import { technologies } from "../../utilities/technologies";
 
 const TechStack = () => {
-  const technologies = [
-    { name: 'HTML 5', icon: 'https://images.icon-icons.com/112/PNG/512/html5_18891.png' },
-    { name: 'CSS', icon: 'https://cdn.worldvectorlogo.com/logos/css-3.svg' },
-    { name: 'JavaScript', icon: 'https://static.vecteezy.com/system/resources/previews/027/127/463/non_2x/javascript-logo-javascript-icon-transparent-free-png.png' },
-    { name: 'Bootstrap', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/960px-Bootstrap_logo.svg.png' },
-    { name: 'React', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/3840px-React-icon.svg.png' },
-    { name: 'JWT', icon: 'https://cdn.worldvectorlogo.com/logos/jwt-3.svg' },
-    { name: 'Npm', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/3840px-Npm-logo.svg.png' },
-    { name: 'Node.js', icon: 'https://cdn.iconscout.com/icon/free/png-256/free-node-js-icon-svg-download-png-1174935.png' },
-    { name: 'Express', icon: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png' },
-    { name: 'MongoDB', icon: 'https://cdn.worldvectorlogo.com/logos/mongodb-icon-1-1.svg' },
-    { name: 'Socket.io', icon: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Socket-io.svg' },
-    { name: 'Github', icon: 'https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png' },
-    { name: 'React Router', icon: 'https://www.svgrepo.com/show/354262/react-router.svg' }
-  ];
-
   return (
-    <div className={classes['full-width-bg']}>
+    <div className={classes["full-width-bg"]}>
       <Container>
-      <Swiper
-        className={classes['tech-slider']}
-        modules={[Autoplay]}
-        spaceBetween={50}
-        slidesPerView={3}
-        loop={true}
-        speed={3000}
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          640: { slidesPerView: 4 },
-          1024: { slidesPerView: 6 },
-        }}
-      >
-        {technologies.map((tech, index) => (
-          <SwiperSlide
-            key={index}
-            className='text-center'
-          >
-            <div
-              className={classes['slide-img-container']}
-            >
-              <img
-                className='w-100 h-100 object-fit-contain d-block'
-                src={tech.icon}
-                alt={tech.name}
-              />
-            </div>
-            <p
-              className='text-secondary m-0 text-nowrap'
-            >{tech.name}</p>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+        <Swiper
+          className={classes["tech-slider"]}
+          modules={[Autoplay]}
+          spaceBetween={50}
+          slidesPerView={3}
+          loop={true}
+          speed={3000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: { slidesPerView: 4 },
+            1024: { slidesPerView: 6 },
+          }}
+        >
+          {technologies.map((tech, index) => (
+            <SwiperSlide key={index} className="text-center">
+              <div className={classes["slide-img-container"]}>
+                <img
+                  className="w-100 h-100 object-fit-contain d-block"
+                  src={tech.icon}
+                  alt={tech.name}
+                />
+              </div>
+              <p className="text-secondary m-0 text-nowrap">{tech.name}</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Container>
     </div>
   );
---
+++ src/utilities/technologies.js
@@ -0,0 +1,83 @@
+export const technologies = [
+  // --- FRONTEND & BASE ---
+  {
+    name: "HTML 5",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/1280px-HTML5_Badge.svg.png",
+  },
+  { name: "CSS", icon: "https://cdn.worldvectorlogo.com/logos/css-3.svg" },
+  {
+    name: "JavaScript",
+    icon: "https://iconape.com/wp-content/files/ez/353342/svg/javascript-seeklogo.com.svg",
+  },
+  {
+    name: "TypeScript",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1280px-Typescript_logo_2020.svg.png",
+  },
+  {
+    name: "Bootstrap",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/960px-Bootstrap_logo.svg.png",
+  },
+  {
+    name: "React",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/3840px-React-icon.svg.png",
+  },
+  /* {
+    name: "React Router",
+    icon: "https://www.svgrepo.com/show/354262/react-router.svg",
+  }, */
+
+  // --- ECOISTEMA JAVA ---
+  {
+    name: "Java",
+    icon: "https://cdn.freebiesupply.com/logos/large/2x/java-14-logo-png-transparent.png",
+  },
+  {
+    name: "Spring Boot",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/1280px-Spring_Boot.svg.png",
+  },
+  {
+    name: "MySQL",
+    icon: "https://cdn.freebiesupply.com/logos/large/2x/mysql-6-logo-png-transparent.png",
+  },
+  /*   {
+    name: "Maven",
+    icon: "https://assets.streamlinehq.com/image/private/w_300,h_300,ar_1/f_auto/v1/icons/4/maven-5fyvzusc6iaxzlgex1ehyh.png/maven-ofzm78ztaq0doqa3ro37lt.png?_a=DATAiZAAZAA0",
+  }, */
+
+  // --- BACKEND JAVASCRIPT & DATABASE ---
+  {
+    name: "Node.js",
+    icon: "https://logodix.com/logo/1764875.png",
+  },
+  {
+    name: "Express",
+    icon: "https://github.com/expressjs.png",
+  },
+  {
+    name: "GraphQL",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1280px-GraphQL_Logo.svg.png",
+  },
+  {
+    name: "MongoDB",
+    icon: "https://cdn.worldvectorlogo.com/logos/mongodb-icon-1-1.svg",
+  },
+  { name: "JWT", icon: "https://cdn.worldvectorlogo.com/logos/jwt-3.svg" },
+  {
+    name: "Socket.io",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/9/96/Socket-io.svg",
+  },
+
+  // --- DEVOPS & STRUMENTI ---
+  /* {
+    name: "Docker",
+    icon: "https://cdn-icons-png.flaticon.com/512/919/919853.png",
+  }, */
+  {
+    name: "Github",
+    icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png",
+  },
+  {
+    name: "Npm",
+    icon: "https://cdn.iconscout.com/icon/free/png-256/free-npm-icon-svg-download-png-14603485.png",
+  },
+];
